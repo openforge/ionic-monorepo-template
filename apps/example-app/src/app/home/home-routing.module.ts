c71244b206e68f9b957bca9798ed67f062a90290
Paulina Gallo <paulina@openforge.io>
2022-03-08 16:08:51 -0500

    build(): copy in standard linters, style, config

--- apps/example-app/src/app/home/home-routing.module.ts
+++ apps/example-app/src/app/home/home-routing.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomePage } from './home.page';
+
+import { HomePageComponent } from './home.page';
 
 const routes: Routes = [
     {
         path: '',
-        component: HomePage,
+        component: HomePageComponent,
     },
 ];
 
